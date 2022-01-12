00ed8a5c3bfa3db38ce5076340d07759a0cc1ccf
Maurin Célian <celianmaurin@hotmail.com>
2022-01-12 15:39:51 +0100

    Connexion via dbconfig.json

--- db_connect.php
+++ db_connect.php
@@ -1,57 +1,34 @@
 <?php
-	class Database{
 
-	private $db_host = 'localhost';
-	private $db_name = 'geoip';
-	private $db_username = 'root';
-	private $password = '';
-	private $charset = 'utf8mb4';
-	
-	public $conn;
-	
-	public function connect(){
-		$this->conn = null;
-		 //test connection
-		if($this->conn == null){
+class Database{
+    private $db;
 
-				try {
-					$server_path = "mysql:host=".$this->getHostName().";dbname=".$this->getDatabaseName().";charset=".$this->getCharacterSet();
-					$pdo_features = [
-							    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-							    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-							    PDO::ATTR_EMULATE_PREPARES   => false,
-							];
-					$this->conn = new PDO($server_path,$this->getUserName(),$this->getPassword());
-				    return $this->conn;
-				} 
-				catch (PDOException $e) {
-					return $e->getMessage();
-				}
+    public function __construct(){
+        $jsonResult = $this->readJSONConfig("dbconfig.json");
+        $this->connection($jsonResult['host'], $jsonResult['databasename'], $jsonResult['user'], $jsonResult['password']);
+    }
 
-	   
-	   }
-		
-	}
+    private function readJSONConfig(string $sDBConfigFile)
+    {
+        $aReturn = array();
+        $sConfig = file_get_contents($sDBConfigFile);
+        $aConfigDB = json_decode($sConfig, true);
+        return $aConfigDB;
+    }
 
-	private function getHostName(){
-		return $this->db_host;
-	}
+    private function connection($host, $databasename, $user, $passworld){
+        try
+        {
+            $this->db = new PDO('mysql:host='.$host.';dbname='.$databasename.';charset=utf8', $user, $passworld);
+        }
+        catch (Exception $e)
+        {
+                die('Erreur : ' . $e->getMessage());
+        }
+    }
 
-	private function getDatabaseName(){
-		return $this->db_name;
-	}
+    public function getConnexion(){
+        return $this->db;
+    }
 
-	private function getUserName(){
-		return $this->db_username;
-	}
-	private function getPassword(){
-		return $this->password;
-	}
-
-	private function getCharacterSet(){
-		return $this->charset;
-	}
-
-	
-	
 }
--- index.php
+++ index.php
@@ -3,14 +3,17 @@
 
 	//Méthode Fonctionnelle mais trop lente pour 3Million de
 	require_once"db_connect.php";
+
 	$db = new Database();
-	$db_conn = $db->connect();
+    $db_conn = $db->getConnexion();
+
     $fileName = "geoip.csv";
 	ini_set('memory_limit', '-1');
 
     if (($handle = fopen($fileName, "r")) !== FALSE) {
-		echo("Importation en cours ...");
+		
 		$time_pre = microtime(true);
+        echo("Importation en cours ...");
         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
             $num = count($data);
             $geoip_array = array();
@@ -26,48 +29,3 @@
         fclose($handle);
 		echo'Importation terminée en ' . $exec_time;
     }
-	
-	
-
-
-	//Test Code Yohann
-			//$conn = mysqli_connect("localhost", "root", "", "geoip");
-    		//$row = 1;
-			//$query = "LOAD DATA INFILE 'geoip.csv' INTO TABLE geoip FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n' (ip_from, ip_to, country_code, country_name, region_name, city_name, latitude, longitude);";
-			//if (!mysqli_query($conn, $query)) {
-    		//printf("Errormessage: %s\n", mysqli_error($conn));
-			//}
-
-	//Test code yann
-			//$connectionString = 'mysql:host=127.0.0.1;dbname=geoip;charset=utf8';
-			//$username = 'root';
-			//$password = '';
-			//ini_set('memory_limit', '-1');
-			//try {
-			//	$mysqlConnection = new PDO($connectionString, $username, $password,
-			//		array(
-			//			PDO::MYSQL_ATTR_LOCAL_INFILE => true,
-			//			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-			//			PDO::ATTR_ORACLE_NULLS => PDO::NULL_NATURAL
-			//		));
-			//} catch (\PDOException $e) {
-			//	throw $e;
-			//}
-			//
-			//function csvToDatabaseImport(PDO $mysqlConnection): void
-			//{
-			//	try {
-			//		$prepareStatement = $mysqlConnection->prepare("LOAD DATA LOCAL INFILE ? INTO TABLE geoip FIELDS TERMINATED BY ? OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY ? (id, ip_from, ip_to, country_code, country_name, region_name, city_name, latitude, longitude) SET id = NULL");
-			//		$prepareStatement->execute(array("geoip.csv", ",", "\n"));
-			//		echo "Done !";
-			//	} catch (\PDOException $e) {
-			//		throw $e;
-			//	}
-			//}
-			//
-			//;
-			//csvToDatabaseImport($mysqlConnection);
-			//$mysqlConnection = null;
-            
-            
-
