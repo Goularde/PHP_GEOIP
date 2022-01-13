23a22c125feac95784a5bf4b0b6eca60a4cf7bab
Maurin Célian <celianmaurin@hotmail.com>
2022-01-13 10:33:36 +0100

    Fin exercice 2 sans les critères

--- locator.php
+++ locator.php
@@ -24,10 +24,15 @@ require_once('db_connect.php');
         $sql = "select country_code,country_name,region_name,city_name,latitude,longitude FROM geoip where ". $ipConverted ." BETWEEN ip_from and ip_to;";
         $statement = $connexion->prepare($sql);
         $result = $statement->execute();
-        echo ($result);
+        // get all publishers
+        $publishers = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($publishers) {
+        	// show the publishers
+        	foreach ($publishers as $publisher) {
+        		echo ($publisher['country_code'] . " | ".$publisher['country_name'] . " | ".$publisher['region_name'] . " | ".$publisher['city_name'] . " | ".$publisher['latitude'] . " | ".$publisher['longitude'] . " | ". "\n");
+        	}
+        }
     }
-
    
-
-    
     geolocalisation();
